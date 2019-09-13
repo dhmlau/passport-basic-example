96c825a6a973dffd9b5f7a0c8388661fab73b317
Diana Lau <dhmlau@ca.ibm.com>
2019-09-13 15:12:43 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Express App with Basic Authentication
+https://github.com/jaredhanson/passport-http
+https://github.com/passport/express-3.x-http-basic-example
+
+Views are getting from https://mherman.org/blog/local-authentication-with-passport-and-express-4/
+
+## Running the application
+```sh
+npm start
+```
---
+++ app.js
@@ -0,0 +1,58 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+// add imports
+var passport = require('passport');
+var Strategy = require('passport-http').BasicStrategy;
+var db = require('./db');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+//passport config
+passport.use(new Strategy(
+  function(username, password, cb) {
+    db.users.findByUsername(username, function(err, user) {
+      if (err) { return cb(err); }
+      if (!user) { return cb(null, false); }
+      if (user.password != password) { return cb(null, false); }
+      return cb(null, user);
+    });
+  }));
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
